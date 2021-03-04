6020534d20c781d19c9d0079722048e1fe6f8aa0
wispfinance <79984916+wispfinance@users.noreply.github.com>
2021-03-04 15:39:01 +0100

    Add files via upload

--- SlimeToken.sol
+++ SlimeToken.sol
@@ -15,7 +15,7 @@ pragma solidity 0.6.12;
 
  
 // CakeToken with Governance.
-contract SlimeToken is BEP20('Slime', 'SLME') {
+contract SlimeToken is BEP20('Wisp', 'WISP') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
